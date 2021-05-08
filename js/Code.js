8c4848e3f0d1c9108018368cadd4773c83430ca4
GarimaKubde <75133096+GarimaKubde@users.noreply.github.com>
2021-05-08 07:25:09 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "REELS";
+const accessCode2 = "STORY";
+const accessCode3 ="COMMENT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("S L E R E", 100,50)
+    fill("lightblue")
+    text("Hint: Social media videos !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("Y O T S R", 700,150)
+fill("lightblue")
+text("Hint: who put in daily !!", 700,170)
+
+fill("white")
+textSize(15)
+text("M E T C M O N", 100,250)
+fill("lightblue")
+text("Hint: Do it when you feel good !!", 100,270)
+}
